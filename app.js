2f7441f4068a0bfdb6dc7b7e3c83ad5429b318d2
JuniorJR2 <ramon22jr31@gmail.com>
2025-10-05 09:06:12 -0600

    Commit inicial: estructura del portafolio

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Rutas
+app.get("/", (req, res) => res.render("index"));
+app.get("/about", (req, res) => res.render("about"));
+app.get("/projects", (req, res) => res.render("projects"));
+app.get("/contact", (req, res) => res.render("contact"));
+
+app.get("/projects/projects1", (req, res) => res.render("projects/projects1"));
+// Servidor
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Servidor en http://localhost:${port}`));
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.ejs",
+    "./views/**/*.html",
+    "./public/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
